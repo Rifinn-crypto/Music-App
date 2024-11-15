2c2da8ce56392d66d14116c23bb39cf996bb4f35
Rifinn-crypto <114596909+Rifinn-crypto@users.noreply.github.com>
2024-11-15 19:10:02 +0400

    Update CustomerDelete.jsx

--- MusicMarketApp/client/src/components/CustomerDelete.jsx
+++ MusicMarketApp/client/src/components/CustomerDelete.jsx
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import api from '../api/api';
 
 function CustomerDelete({ id, onDeleteSuccess }) {
+  const [message, setMessage] = useState(''); // Добавляем состояние для сообщения
+
   const handleDelete = async () => {
     try {
       await api.delete(`/customer/${id}`);
-      onDeleteSuccess(id); // Notify the parent component (CustomerList) to remove the customer from the list
+      onDeleteSuccess(id);
+      setMessage('Customer deleted successfully!');
     } catch (error) {
-      console.error('Error deleting customer:', error);
-      alert('Failed to delete customer');
+      setMessage('Failed to delete customer');
     }
   };
 
   return (
-    <button onClick={handleDelete} className="btn btn-danger btn-sm">
-      Delete
-    </button>
+    <div>
+      {/* Выводим сообщение в зависимости от типа */}
+      {message && (
+        <div className={`alert ${message.includes('Failed') ? 'alert-danger' : 'alert-success'}`} role="alert">
+          {message}
+        </div>
+      )}
+      <button onClick={handleDelete} className="btn btn-danger btn-sm">
+        Delete
+      </button>
+    </div>
   );
-};
+}
 
 export default CustomerDelete;
